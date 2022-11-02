8879bcce9aee8d5adb0d69ad7435bb778c391ca3
pankaj1707k <pankaj.1707k@gmail.com>
2022-11-02 16:53:07 +0530

    feat: create model User

--- base/settings.py
+++ base/settings.py
@@ -127,3 +127,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Authentication model
+AUTH_USER_MODEL = "users.User"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    """A custom user model with email field required and unique"""
+
+    email = models.EmailField(_("email"), unique=True)
