636ab1850814d10daaa8f7a1d10cb29b0cd9b92c
pankaj1707k <pankaj.1707k@gmail.com>
2022-11-02 22:21:33 +0530

    feat: add Post model and register it on admin site

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from posts.utils import get_post_image_path
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(_("image"), upload_to=get_post_image_path)
+    created_on = models.DateTimeField(_("created on"), auto_now_add=True)
+    updated_on = models.DateTimeField(_("updated on"), auto_now=True)
+
+    class Meta:
+        ordering = ["-updated_on"]
+
+    def __str__(self) -> str:
+        return f"{self.id}__{self.author.username}"
