3927974ce8d4bb261b525441f479fa7222159d4d
pankaj1707k <pankaj.1707k@gmail.com>
2022-11-03 20:08:48 +0530

    feat: post update

--- posts/urls.py
+++ posts/urls.py
@@ -5,4 +5,5 @@ from posts import views
 urlpatterns = [
     path("feed/", views.PostListView.as_view(), name="feed"),
     path("<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
+    path("<int:pk>/update/", views.PostUpdateView.as_view(), name="post-update"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,9 @@
-from django.views import generic
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import redirect, render
+from django.views import View, generic
 
+from posts.forms import PostForm
 from posts.models import Post
 
 
@@ -14,3 +18,14 @@ class PostDetailView(generic.DetailView):
     template_name = "posts/post_detail.html"
     model = Post
     context_object_name = "post"
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
+    model = Post
+    template_name = "posts/post_form.html"
+    form_class = PostForm
+    success_url = "/"
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
